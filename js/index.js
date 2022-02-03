c4af14903493dd086dc3bc4037209ed6d66aae4e
Foebry <rain_fabry@hotmail.com>
2022-02-03 22:57:35 +0100

    gegenereerde lijst verdwijnt op pas na geselecteerde waarde vastgezet is

--- js/index.js
+++ js/index.js
@@ -7,8 +7,8 @@ let button_add;
 
 const handleEvents = function(e) {
 	if (e.target.id == "button_add") add_row(e);
-	else if (e.target.classList.contains("input__store") || e.target.classList.contains("input__article") || e.target.classList.contains("input__aantal") || e.target.classList.contains("input__prijs")) handleInput(e);
 	else if (e.target.classList.contains("store__list__item") || e.target.classList.contains("article__list__item")) setValue(e);
+	else if (e.target.classList.contains("input__store") || e.target.classList.contains("input__article") || e.target.classList.contains("input__aantal") || e.target.classList.contains("input__prijs")) handleInput(e);
 	else if (e.target.classList.contains("button__detail")) redirect(e);
 }
 
@@ -27,43 +27,69 @@ const add_row = function(e) {
 const handleInput = function(e) {
 	if (Object.values(e.target.attributes).filter(el => el.value == "readonly").length == 0) {
 		e.target.addEventListener("input", updateValue);
-		e.target.addEventListener("blur", looseFocus);
-		e.target.addEventListener("focus", updateValue);
+		e.target.addEventListener("focus", setFocus);
+		e.target.addEventListener("blur", looseFocus, 100);
 	}
 }
 
-const updateValue = function() {
-	if (this.value.length > 0 && (this.classList.value.includes("input__article") || this.classList.value.includes("input__store"))) {
-		const options_list = this.parentElement.children[2];
-		const class_list = this.classList.value;
-		const list = class_list.includes("input__article") ? articles : class_list.includes("input__store") ? stores : null;
-		options_list.classList.remove("list--hidden");
-		options_list.innerHTML = Array(...list.children).filter(el => el.outerText.toLowerCase().includes(this.value.toLowerCase())).slice(0, 10).reduce((str, el) => str + el.outerHTML, "");
+/*if (this.classList.value.includes("input__article") || this.classList.value.includes("input__store")) {
+				//console.log("leaving input__article or input__store");
+				this.nextElementSibling.classList.add("list--hidden");
+				//console.log(e.target);
+				//console.log(this);
+				if (!e.target.classList.contains("store__list__item") && !e.target.classList.contains("article__list__item")) {
+					this.nextElementSibling.innerHTML = "";
+				}
+			}
+		}
+	)
+}
+}
+}*/
+
+const setFocus = function(e) {
+	const options_list = e.target.parentElement.children[2];
+	const class_list = e.target.classList.value;
+	const list = class_list.includes("input__article") ? articles : class_list.includes("input__store") ? stores : null;
+	if ((e.target.value.length > 0) && (e.target.classList.value.includes("input__store") || e.target.classList.value.includes("input__article"))) {
+		options_list.style.display = "block";
+		options_list_items = Array(...list.children).filter(el => el.outerText.toLowerCase().includes(e.target.value.toLowerCase())).slice(0, 10);
+		options_list_items.forEach(el => el.onclick = setValue);
+		options_list.innerHTML = options_list_items.reduce((str, el) => str + el.outerHTML, "");
+	} else if (e.target.value.length == 0) {
+		options_list.innerHTML = "";
+		options_list.classList.add("list--hidden");
 	}
-	this.setAttribute("value", this.value);
 }
 
-const looseFocus = function() {
-	if (this.classList.value.includes("input__article") || this.classList.value.includes("input__store")) {
-		this.nextElementSibling.classList.add("list--hidden");
-	}
+const updateValue = function(e) {
+	e.target.setAttribute("value", e.target.value);
+	setFocus(e);
+}
+
+const removeListItems = function(e) {
+	e.target.nextElementSibling.style.display = "none";
+}
+
+const looseFocus = function(e) {
+	setTimeout(removeListItems, 100, e);
 }
 
 const setValue = function(e) {
-	const detail_button = e.target.parentElement.parentElement.parentElement.children[4].children[0];
-	//console.log(detail_button.attributes[3].value);
-	const input_field = e.target.parentElement.previousElementSibling;
-	const select_field = e.target.parentElement.parentElement.children[0].children[0];
+	if (e.target.classList.contains("store__list__item") || e.target.classList.contains("sotre__list__item")) {
+		const detail_button = e.target.parentElement.parentElement.parentElement.children[4].children[0];
+		const input_field = e.target.parentElement.previousElementSibling;
+		const select_field = e.target.parentElement.parentElement.children[0].children[0];
 
-	input_field.setAttribute("value", e.target.innerHTML);
-	input_field.value = e.target.innerHTML;
-	select_field.value = e.target.id;
-	if (e.target.parentElement.classList.contains("article_list")) {
-		detail_button.attributes[3].value += e.target.id;
-		detail_button.removeAttribute("disabled");
+		input_field.setAttribute("value", e.target.innerHTML);
+		input_field.value = e.target.innerText;
+		console.log(e);
+		select_field.value = e.target.id;
+		if (e.target.parentElement.classList.contains("article_list")) {
+			detail_button.attributes[3].value += e.target.id;
+			detail_button.removeAttribute("disabled");
+		}
 	}
-	e.target.parentElement.innerHTML = "";
-	console.log(detail_button);
 }
 
 const redirect = function(e) {
