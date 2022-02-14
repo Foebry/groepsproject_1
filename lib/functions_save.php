4ec63fbec87b7010f851665c35b04668b13ca601
Foebry <rain_fabry@hotmail.com>
2022-02-14 10:44:19 +0100

    cache + refer

--- lib/functions_save.php
+++ lib/functions_save.php
@@ -12,3 +12,51 @@
         return $_SESSION["boodschappen"][$gro_id]["headers"];
 
     }
+
+    /**
+    * Opdat de gebruiker zijn aan te maken boodschap niet zou verliezen moest deze navigeren naar
+    * de detail pagina van een bepaald product, wordt de boodschap gecached vanaf het moment dat
+    * de gebruiker het boodschap_detail formulier inzendt.
+    */
+    function cacheGrocery(int $gro_id) :void{
+        //overloop alle rijen van de ingezonde boodschap
+        foreach($_POST["data"] as $row_id => $data){
+            //is een rij helemaal leeg, verijder de rij volledig
+            //geen nut om een lege rij te onthouden
+            if (count(array_unique($data)) == 1 && current($data) == ""){
+                unset($_POST["data"][$row_id]);
+                continue;
+            }
+            $_POST["data"][$row_id]["row_id"] = $row_id;
+            $_POST["data"][$row_id]["row_art_id"] = (int) $_POST["data"][$row_id]["row_art_id"];
+            $_POST["data"][$row_id]["row_sto_id"] = (int) $_POST["data"][$row_id]["row_sto_id"];
+            $_POST["data"][$row_id]["row_gro_id"] = (int) $gro_id;
+            $_POST["data"][$row_id]["row_pric"] = (float) $_POST["data"][$row_id]["row_pric"];
+        }
+        //bevat het ingezonden formulier geen data of is na verwijderen van lege rijen de data
+        // een lege array geworden (enkel lege rijen ingezonden).
+        // keer terug, want dit wordt niet aanvaard.
+        if(!key_exists("data", $_POST) || count($_POST["data"]) == 0){
+            $_POST["data"] = [];
+            $_SESSION["errors"]["data_error"] = "Uw boodschap kan mag niet leeg zijn. Gelieve een artikel in te vullen";
+        }
+
+        // cache doorgestuurde headers (info over de boodschap) en data (info over de verschillende rijen)
+        $_SESSION["boodschappen"][$gro_id]["headers"][0] = $_POST["headers"];
+        $_SESSION["boodschappen"][$gro_id]["data"] = $_POST["data"];
+        $_SESSION["next_row_id"] = $_POST["headers"]["next_row_id"] + 1;
+    }
+
+
+    /**
+    *
+    */
+    function deleteFromCache() :int{
+        if ($_POST["form"] == "boodschapdetail"){
+            // verwijder de row-record uit de $_SESSION cache
+            $row_id = (int) explode("-", $_POST["action"])[1];
+            unset($_SESSION["boodschappen"][$gro_id]["data"][$row_id]);
+
+            return $row_id;
+        }
+    }
--- lib/save.php
+++ lib/save.php
@@ -6,50 +6,31 @@ validateCSRF();
 
 if ($_POST["form"] == "boodschapdetail"){
     $gro_id = $_POST["headers"]["gro_id"];
+    cacheGrocery($gro_id);
+}
 
-    // lege rijen verwijderen
-    foreach($_POST["data"] as $row_id => $data){
-        if(count(array_unique($data)) == 1 && current($data) == ""){
-            unset($_POST["data"][$row_id]);
-            continue;
-        }
-        $_POST["data"][$row_id]["row_id"] = $row_id;
-        $_POST["data"][$row_id]["row_art_id"] = (int) $_POST["data"][$row_id]["row_art_id"];
-        $_POST["data"][$row_id]["row_sto_id"] = (int) $_POST["data"][$row_id]["row_sto_id"];
-        $_POST["data"][$row_id]["row_gro_id"] = (int) $gro_id;
-        $_POST["data"][$row_id]["pri_value"] = (float) $_POST["data"][$row_id]["row_pric"];
-    }
-    // Boodschap mag niet leeg zijn
-    if (!key_exists("data", $_POST) || count($_POST["data"]) == 0){
-        $_POST["data"] = [];
-        $_SESSION["errors"]["data_error"] = "Uw boodschap kan helaas niet leeg zijn. Gelieve een artikel in te vullen";
-    }
+// Gebruiker wil navigeren naar een andere de detail pagina van een product.
+if ($_POST["action"] == "refer"){
+    exit(header("location:".$_POST["refer"]));
+}
 
-    // boodschapdetail in $_SESSION opslaan
-    $_SESSION["boodschappen"][$gro_id]["headers"][0] = $_POST["headers"];
-    $_SESSION["boodschappen"][$gro_id]["data"] = $_POST["data"];
-    $_SESSION["next_row_id"] = $_POST["headers"]["next_row_id"] +1;
+// Gebruiker wil een row-record verwijderen
+if( strpos($_POST["action"], "delete") !== false){
+    $table = $_POST["table"];
+    $key = $_POST["key"];
+    // Gebruiker wil boodschapdetail regel verwijderen
+    $row_id = deleteFromCache();
 
+    $id = $row_id ? $row_id : null;
 
-    // indien de gebruiker wil navigeren naar een andere pagina.
-    if ($_POST["action"] == "refer"){
-        exit(header("location:".$_POST["refer"]));
-    }
-    //indien de gebruiker een row-record wil verwijderen
-    elseif( strpos($_POST["action"], "delete") !== false){
+    // verwijder record uit de databank
+    $sql_delete = "delete from $table where $key = $id";
+    ExecuteSQL($sql_delete);
 
-        // verwijder de row-record uit de $_SESSION cache
-        $row_id = (int) explode("-", $_POST["action"])[1];
-        unset($_SESSION["boodschappen"][$gro_id]["data"][$row_id]);
-
-        // verwijder de row-record uit de databank
-        $sql_delete = "delete from row where row_id = $row_id";
-        ExecuteSQL($sql_delete);
-
-        // zet een melding voor de gebruiker en keer terug naar het formulier
-        $_SESSION["info"]["msg"] = $_POST["info-delete"];
-        exit(header("location:".$_SERVER["HTTP_REFERER"]));
-    }
+    // zet een melding voor de gebruiker en keer terug naar het formulier
+    $_SESSION["info"]["msg"] = $_POST["info-delete"];
+    exit(header("location:".$_SERVER["HTTP_REFERER"]));
+}
 
     // inzending van het formulier
     elseif ($_POST["action"] == "submit"){
@@ -119,7 +100,7 @@ if ($_POST["form"] == "boodschapdetail"){
         // navigeer naar homepagina
         exit(header("location:".$_POST["refer"]));
     }
-}
+
 else{
     $sql_statements = [];
     $table = $_POST["table"];
@@ -214,89 +195,4 @@ function checkGroceryForItemStoreCombination(){
     $_SESSION["next_row_id"] += 1;
 
     return False;
-}
-
-
-
-
-
-
-
-SaveFormData();
-
-function SaveFormData()
-{
-    if ( $_SERVER['REQUEST_METHOD'] == "POST" )
-    {
-        //sanitization
-        $_POST = StripSpaces($_POST);
-        $_POST = ConvertSpecialChars($_POST);
-
-        $table = $pkey = $update = $insert = $where = $str_keys_values = "";
-
-        //get important metadata
-        if ( ! key_exists("table", $_POST)) die("Missing table");
-        if ( ! key_exists("pkey", $_POST)) die("Missing pkey");
-
-        $table = $_POST['table'];
-        $pkey = $_POST['pkey'];
-
-        //validation
-        $sending_form_uri = $_SERVER['HTTP_REFERER'];
-        CompareWithDatabase( $table, $pkey );
-
-        //terugkeren naar afzender als er een fout is
-        if ( count($_SESSION['errors']) > 0 ) { header( "Location: " . $sending_form_uri ); exit(); }
-
-        //insert or update?
-        if ( $_POST["$pkey"] > 0 ) $update = true;
-        else $insert = true;
-
-        if ( $update ) $sql = "UPDATE $table SET ";
-        if ( $insert ) $sql = "INSERT INTO $table SET ";
-
-        //make key-value string part of SQL statement
-        $keys_values = [];
-
-        foreach ( $_POST as $field => $value )
-        {
-            //skip non-data fields
-            if ( in_array( $field, [ 'table', 'pkey', 'afterinsert', 'afterupdate', 'csrf' ] ) ) continue;
-
-            //hashing password
-            if ($field == "user_password"){
-                $value =  password_hash($value,PASSWORD_DEFAULT);
-            }
-
-            //handle primary key field
-            if ( $field == $pkey )
-            {
-                if ( $update ) $where = " WHERE $pkey = $value ";
-                continue;
-            }
-
-            //all other data-fields
-            $keys_values[] = " $field = '$value' " ;
-        }
-
-        $str_keys_values = implode(" , ", $keys_values );
-
-        //extend SQL with key-values
-        $sql .= $str_keys_values;
-
-        //extend SQL with WHERE
-        $sql .= $where;
-
-        //run SQL
-        $result = ExecuteSQL( $sql );
-
-        //output if not redirected
-        print $sql ;
-        print "<br>";
-        print $result->rowCount() . " records affected";
-
-        //redirect after insert or update
-        if ( $insert AND $_POST["afterinsert"] > "" ) header("Location: ../" . $_POST["afterinsert"] );
-        if ( $update AND $_POST["afterupdate"] > "" ) header("Location: ../" . $_POST["afterupdate"] );
-    }
 }
