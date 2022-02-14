393bff99b046c536072bf43d9daf0a29f662d48a
Tijl P <tijlp.business@gmail.com>
2022-02-14 09:38:00 +0100

    styling responsive

--- index.php
+++ index.php
@@ -43,11 +43,7 @@ $output = MergeViewWithData( 'boodschapcard.html', $data );
 $template = file_get_contents("templates/boodschappen.html");
 $result .= str_replace("@list@", $output, $template);
 $result = str_replace("@next_gro_id@", $_SESSION["next_gro_id"], $result);
-<<<<<<< HEAD
 $result = removeEmptyPlaceholders($result);
-=======
-$result = mergeErrorInfoPlaceholders($result, $errors, $info);
->>>>>>> dev
 
 print $result;
 PrintFooter();
